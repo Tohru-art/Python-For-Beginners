8aa0cce0c3617b66ce9217ef9999565ebbd24d93
Will-Garlens Pierre <willgarlensp@gmail.com>
2025-08-24 00:46:50 -0400

    More Booleans

---
+++ Booleans/main.py
@@ -0,0 +1,31 @@
+day = 21 
+weight = 190.5
+month = 'October'
+
+light_is_on = True
+
+if light_is_on:
+    print("The light is on!")
+
+#Challenge:Find something around you that could be represented by a Boolean and make a variable for it
+
+door_is_open = False
+
+if door_is_open:
+    print("The door is open!")
+else:
+    print("The door is closed!")
+
+weight = 190.5
+
+if weight < 195:
+    print("You're under weight:)")
+
+#Challenge: Make a variable age, and if someone is 18 y.o or older, print that they are adult. Otherwise print that they are a child
+
+age = 23
+
+if age < 18:
+    print("You are a child")
+else:
+    print("You are an adult!")
