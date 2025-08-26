6676a695c5cfaec5452fcf083d3c2bd4ecdbfa0c
Will-Garlens Pierre <willgarlensp@gmail.com>
2025-08-26 03:04:39 -0400

    Add OddOrEven.py challenge

---
+++ Booleans/OddOrEven.py
@@ -0,0 +1,8 @@
+# OddOrEven.py
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
--- Booleans/main.py
+++ Booleans/main.py
@@ -19,7 +19,7 @@ else:
 weight = 190.5
 
 if weight < 195:
-    print("You're under weight:)")
+    print("You're under weight:)") 
 
 #Challenge: Make a variable age, and if someone is 18 y.o or older, print that they are adult. Otherwise print that they are a child
 
